dc08e0dbc0db49f03d79f1a44a0c4494bd5dd3a0
chshrcat <scorpion9595@mail.ru>
2015-12-24 09:01:57 +0600

    Исправить ошибка 1514

--- ViselnikGame/Game.cs
+++ ViselnikGame/Game.cs
@@ -33,10 +33,12 @@ namespace OmGTU.Advance.Profit.Loyal.ViselnikGame
         /// Create files of words.
         /// </summary>
         public static string[] Animals = File.ReadAllLines(@"Resources\animals.txt");
+
         /// <summary>
         ///объект рандом который  задает индекс элемента массива  Words.
         /// </summary>
         Random rand = new Random();
+
         /// <summary>
         /// массив индексов 
         /// </summary>
@@ -98,6 +100,7 @@ namespace OmGTU.Advance.Profit.Loyal.ViselnikGame
 
         
         int count = 0;
+
         /// <summary>
         /// Функция присваиваюшая длине выводимого массива длину слова и  заполняющего выводимый массив знаком "-".
         /// </summary>
